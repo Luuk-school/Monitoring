f16cca7d762455b2a61573c9e46648513831506a
Luuk <551367@student.fontys.nl>
2025-11-04 18:12:36 +0100

    Projecter herstart, van SSH--> get/post gegaan. eerst werkende flask website gemaakt.

---
+++ Agent_manager/agentchecker.py
@@ -0,0 +1,2 @@
+#verwerk hier een get request naar de agent to checken of die online + versie
+import requests
---
+++ MO_server/main.py
@@ -0,0 +1,4 @@
+import flaskWebsite
+
+# start the Flask server
+flaskWebsite.app.run(host='192.168.2.5')               #vraag aan bjorn hoe dit werkt en waarom het werkt??????
